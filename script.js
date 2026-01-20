6ef2c3d9e2739d25969b913a9c1356bae2685cca
jsu <jsu@student.42singapore.sg>
2026-01-20 18:51:00 +0800

    feat: (1) switched from text input, to MCQ input & (2) added a 'try again' button

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let timeLeft = 120; // timeLeft in seconds, should be > 0
-let maxPokemonsPerGame = 100; // valid values, should range from 1 to 1025
+// values for "macros"
+let timeLeft = 60; // timeLeft in seconds, value should be > 0
+let pokemonsPerQuestion = 3;
+
+// labels for document elements, for easier referral in code
+const timerDisplay = document.getElementById('countdown-timer');
+const pokemonImage = document.getElementById('pokemon-image');
+const mcqButtons = document.getElementById('options');
+const resultElement = document.getElementById('result');
+const tryAgainButton = document.getElementById('reloadButton');
+
+// game-level vars
+let score = 0;
+let pokemonsEncountered = 1;
+
+// question-level vars
+let mcqOptions = [];
+let option_1_name = '';
+let option_2_name = '';
+let correctPokemonName = ''; // option_3_name
 
 const countdownInterval = setInterval( () => {
   // Calculate minutes and seconds
@@ -17,21 +35,26 @@ const countdownInterval = setInterval( () => {
 	if (timeLeft < 0) {
 		clearInterval(countdownInterval); // stop the countdown timer
 		timerDisplay.textContent = 'Time\'s up!';
-		// TODO, render next view for game, eg. show who won/lose/draw
-		// For now, I just "block" the player from entering input/click submit
-		guessInput.disabled = true;
-		mySubmitButton.disabled = true;
 
-		// tested and found that if I stayed at an incorrect answer state,
-		// BEFORE the timer finished, I could continue to click 'Next Pokemon' button!
-		nextPokemonButton.disabled = true;  
+		// need to "clear the screen" by removing mcqButtons
+		// otherwise, user can still submit one last guess, AFTER time is up!
+		mcqButtons.innerHTML = '';
+		resultElement.textContent = '';
+
+		// TO DO, call API, to submit scores for current user, for user stats persistence
+		//console.log("call a BE API, to submit stats for this game, to be logged into a db");
+
+		// unhide/show "Try Again" button
+		tryAgainButton.style.display = 'block';
 	}
 		
 }, 1000); // update every 1000 milliseconds (1 second)
 
+function reloadPage() {
+	window.location.reload();
+}
 
-// generate a list (subset) of pokemons from all possible pokemons
-// no repeats
+// generate a subset of pokemon IDs, from all possible pokemons; no repeats
 function selectUniqueSubset(min, max, subsetSize) {
   // 1. Create an Array of the Range
   const rangeArray = Array.from({ length: max - min + 1 }, (_, i) => min + i);
@@ -50,139 +73,153 @@ function selectUniqueSubset(min, max, subsetSize) {
   return shuffledArray.slice(0, subsetSize);
 } 
 
-// uniqueNumbers contains the ID numbers of N = subsetSize of pokemons, for current game
-// to be "injected" into each client's front-end,
-// eg. for remote / multiplayer play modes
-const uniqueNumbers = selectUniqueSubset(1, 1025, maxPokemonsPerGame); // currently set to 100
-//console.log(uniqueNumbers);
-
-/*
- * to do / need to fix?
- * or just hardcode subsetSize = 1025, and include an easter egg / secret msg
- * if a player actually goes thru all 1025 pokemons in a single game?
- *
- * Possible error emerges when,
- * a player manages to pop off all the elements from uniqueNumbers array
- * in fetchRandomPokemon()
- */
-
-
-// for shorter/more convenient variable referral, in code
-const pokemonImage = document.getElementById('pokemon-image');
-const guessInput = document.getElementById('guess-input');
-const message = document.getElementById('message');
-const mySubmitButton = document.getElementById("submit-guess");
-const nextPokemonButton = document.getElementById('next-pokemon');
-const timerDisplay = document.getElementById('countdown-timer');
-
-
-let correctPokemonName = ''; // global, so can be shared by fetchRandomPokemon() & show-name/hintMsg
-let score = 0;
-let pokemonsEncountered = 0;
-
-/*
- * to check whether game is working:
- * open console, to see NAME of currently displayed pokemon
- *
- * to "switch off" this "debug" mode,
- * comment out the line, console.log(data.name), in fetchRandomPokemon()
- *
- * to double-confirm names, visit bulbapedia
- * https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number
- */
-async function fetchRandomPokemon() {
-	
-	const randomId = uniqueNumbers.pop(); // get last element uniqueNumbers array 
-	//console.log(randomId);
-  const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + randomId;
+// valid values for "id", range from 1 to 1025, inclusive
+async function fetchPokemonName(id) { 
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + id;
 
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    displayPokemon(data);
-    correctPokemonName = data.name.toLowerCase();
-    
-    console.log(data.name); // for debugging only, pls do NOT use to cheat!
-    
+    const name = data.name;
+		
+		mcqOptions.push(name);
+		console.log("fpn(): ", id, name); // debug
+
   } catch (error) {
-    console.error('Error fetching Pokémon:', error);
-    message.textContent = 'Failed to load a Pokémon. Please try again.';
+    console.error("Error fetching Pokémon's name: ", error);
   }
 }
 
-function displayPokemon(data) {
-  const imageUrl = data.sprites.front_default;
-  pokemonImage.src = imageUrl;
-  guessInput.value = '';
-  message.textContent = '';
+async function fetchPokemonImg(id) {
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + id;
+
+  try {
+		// Step 1 of 3:
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+		// set the value for image src, for the document.element in the FE
+		pokemonImage.src = data.sprites.front_default;
+
+		// set the correct answer, to compare with submitted option
+		correctPokemonName = data.name;
+		console.log("fpi(): ", id, correctPokemonName);
+
+		// add correct answer, to mcqOptions
+		mcqOptions.push(correctPokemonName);
+  } catch (error) {
+    console.error("Error fetching Pokémon image: ", error);
+  }
 }
 
-// 2 functions to update stats & display the updated stats in web page
-// purely client-side!
-function updateScoreDisplay() {
-	document.getElementById("score-display").textContent = score;
-	document.getElementById("pokemons-encountered").textContent = pokemonsEncountered;
+async function generateQuestion() {
+	mcqButtons.innerHTML = ''; // clear buttons (for mcqOptions) on FE
+	mcqOptions.length = 0; // clear array for "mcqOptions"
+
+	console.log("starting 3rd party API calls...");
+
+	let pokemonIds = selectUniqueSubset(1, 1025, pokemonsPerQuestion);
+
+	let id1= pokemonIds.pop();
+	let id2= pokemonIds.pop();
+	let id3= pokemonIds.pop();
+
+	const promises = [
+		fetchPokemonImg(id1), // only "correct" answer, needs to D/L image 
+		fetchPokemonName(id2), // rest only needs the name to be "pulled"
+		fetchPokemonName(id3) // from 3rd party API
+	];
+	
+	try {
+		// wait, for the "previous" bunch of necessary API calls 
+		const results = await Promise.all(promises);
+		console.log("3rd party API calls were successful!");
+
+		/*
+		 * IMPORTANT, we should only call the functions below,
+		 * AFTER entire array of promises, ie. "results" has resolved
+		 * Spent a long time debugging,
+		 * only to finally realize that NOT doing the async part correctly,
+		 * was jamming me up...
+		 * T_T
+		 *
+		 * However, there is one advantage of the async approach:
+		 * The order of elements in mcqOptions is "randomized"!
+		 * This is because the 3 async calls will NOT always resolve in the order
+		 *
+		 * This also meant that the correct mcq option,
+		 * could be in any position, ie. randomized
+		 * when presented to the user in the FE!
+		 */
+		
+		// creates a button for each mcq option
+		mcqOptions.forEach((option, index) =>
+			{
+				const button = document.createElement('button');
+				button.textContent = option;
+				button.addEventListener('click', () =>
+					{
+						// once user clicks an option,
+						// we should remove the availble options,
+						// in case they try to click some more; see rate limiting
+						mcqButtons.innerHTML = '';
+						checkAnswer(index);
+					}
+				);
+				mcqButtons.appendChild(button);
+			}
+		);	
+	} catch (error) {
+		console.error("One of the API calls, failed...", error);
+	}
 }
 
-function increaseScore(points) {
-	score += points;
+function checkAnswer(index) {
+	//console.log("submitted:", index, mcqOptions[index]);
+	let correctFlag = 0;
+
+	if (mcqOptions[index] === correctPokemonName) {
+		correctFlag = 1;
+		score++;
+		resultElement.textContent = "Correct!";
+		console.log("Correct!");
+		
+	} else {
+		let feedback = "Wrong... Correct answer is " + correctPokemonName;
+		resultElement.textContent = feedback;
+		console.log("Wrong... Correct answer is", correctPokemonName);
+	}
+		
 	pokemonsEncountered++;
 	updateScoreDisplay();
+
+	// default is 1 second delay, to show result of choice
+	// option to vary the delay for correct/wrong submission
+	if (timeLeft > 0) {
+			if (correctFlag) {
+				setTimeout(() => {
+					resultElement.textContent = "";
+					generateQuestion();
+				}, 1000);
+			} else {
+				setTimeout(() => {
+					resultElement.textContent = "";
+					generateQuestion();
+				}, 1000);
+			}
+		}
 }
 
-function checkGuess() {
-  const userGuess = guessInput.value.toLowerCase();
-  if (userGuess === correctPokemonName) {
-    message.textContent = 'Correct! You got it.'; // will probably flash past too quickly, hard for most people to see
-
-    increaseScore(1); // increment score by 1
-
-    fetchRandomPokemon();
-
-	} else {
-		/*
-		 * Player has to manually click "next pokemon button" to proceed
-		 * This is intentional
-		 * So that player can take their time to learn, ie. match picture to name
-		 */
-		message.textContent = userGuess + ' is incorrect! The correct answer is: ' + correctPokemonName;
-
-		increaseScore(0); // increment score by zero
-
-		/*
-		 * block player from either: (
-		 * 1) altering their entered input, or
-		 * 2) resubmitting their input
-		 *
-		 * Previously, it WAS possible for a player to alter their previous answer, and/or click submit, again and again... 
-		 */
-		guessInput.disabled = true;
-		mySubmitButton.disabled = true; 
-
-		// reveal the "Next Pokemon" button
-		nextPokemonButton.style.display = 'inline'; 
-  }
+function updateScoreDisplay() {
+	document.getElementById("pokemons-encountered").textContent = pokemonsEncountered;
+	document.getElementById("score-display").textContent = score;
 }
 
-mySubmitButton.addEventListener('click', checkGuess);
-nextPokemonButton.addEventListener('click', () => {
-
-	// re-enable input field, and submit button
-	guessInput.disabled = false;
-	mySubmitButton.disabled = false;
-
-	// hide the "Next Pokemon" button
-  nextPokemonButton.style.display = 'none';
-
-  fetchRandomPokemon();
-});
-
-// Run the game for the first time
-fetchRandomPokemon();
-console.log('Gotta ketchum all!')
+console.log('Gotta ketchum all!');
+generateQuestion(); // generate the first question
 
 /*
-last updated 2025_12_09
+last updated 2026_01_02
 
 changelog
 
@@ -192,4 +229,11 @@ v3, added a score counter
 v4, disable input field for incorrect answer + re-enable input field when 'next-pokemon' button is pressed
 v5, added pokemonsEncountered counter
 v6, added countdown timer
+v7, refactored fetchRandomPokemon() + displayPokemon() into fetchPokemonImg(), fetchPokemonName(), & generateQuestion()
+also figured out how to await a bunch of "necessary" API calls, BEFORE calling next function
+v8, added dynamic generation of buttons for (currently N == 3) mcqOptions on FE +
+complementary checkAnswer() w/ option for variable feedback delay
+v9, ensure that AFTER user clicks on an option, removed mcqOptions from FE,
+also ensured that after timeOver, mcqOptions are removed, to prevent one last try, that can take forever
+v10, added a "try again" button (and its supporting functionality) so that after the time runs out, a user can try again, WITHOUT having to refresh the webpage via the browser
 */
